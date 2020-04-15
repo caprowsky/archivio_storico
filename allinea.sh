6eba9d19102198fe49e267b528db97764faa1ea8
sergej <sergio.durzu@gmail.com>
2020-04-15 12:41:00 +0200

    Added search api solr module and confs

--- allinea.sh
+++ allinea.sh
@@ -5,8 +5,12 @@ docker-compose down;
 docker-compose up -d;
 
 sleep 20
-
+docker exec -ti studenti_solr make create core=default -f /usr/local/bin/actions.mk
 drush updb -y;
 drush cim -y;
+
 drush cr;
-#docker exec -ti ilminutodrupal_solr make create core=default -f /usr/local/bin/actions.mk;
+drush search-api-clear
+drush search-api-index
+
+
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -35,6 +35,7 @@ class ComposerStaticInit2f77adf49a2df08b2cd6d1bd2b067b58
     public static $prefixLengthsPsr4 = array (
         'Z' => 
         array (
+            'ZipStream\\' => 10,
             'Zend\\Stdlib\\' => 12,
             'Zend\\Feed\\' => 10,
             'Zend\\Escaper\\' => 13,
@@ -63,7 +64,10 @@ class ComposerStaticInit2f77adf49a2df08b2cd6d1bd2b067b58
             'Symfony\\Polyfill\\Mbstring\\' => 26,
             'Symfony\\Polyfill\\Iconv\\' => 23,
             'Symfony\\Polyfill\\Ctype\\' => 23,
+            'Symfony\\Contracts\\Service\\' => 26,
+            'Symfony\\Contracts\\Cache\\' => 24,
             'Symfony\\Component\\Yaml\\' => 23,
+            'Symfony\\Component\\VarExporter\\' => 30,
             'Symfony\\Component\\VarDumper\\' => 28,
             'Symfony\\Component\\Validator\\' => 28,
             'Symfony\\Component\\Translation\\' => 30,
@@ -79,8 +83,10 @@ class ComposerStaticInit2f77adf49a2df08b2cd6d1bd2b067b58
             'Symfony\\Component\\Debug\\' => 24,
             'Symfony\\Component\\Console\\' => 26,
             'Symfony\\Component\\ClassLoader\\' => 30,
+            'Symfony\\Component\\Cache\\' => 24,
             'Symfony\\Cmf\\Component\\Routing\\' => 30,
             'Symfony\\Bridge\\PsrHttpMessage\\' => 30,
+            'Solarium\\' => 9,
             'SelfUpdate\\' => 11,
         ),
         'R' => 
@@ -93,10 +99,12 @@ class ComposerStaticInit2f77adf49a2df08b2cd6d1bd2b067b58
             'Psr\\Log\\' => 8,
             'Psr\\Http\\Message\\' => 17,
             'Psr\\Container\\' => 14,
+            'Psr\\Cache\\' => 10,
             'PhpParser\\' => 10,
         ),
         'M' => 
         array (
+            'MyCLabs\\Enum\\' => 13,
             'Masterminds\\' => 12,
         ),
         'L' => 
@@ -163,6 +171,10 @@ class ComposerStaticInit2f77adf49a2df08b2cd6d1bd2b067b58
     );
 
     public static $prefixDirsPsr4 = array (
+        'ZipStream\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/maennchen/zipstream-php/src',
+        ),
         'Zend\\Stdlib\\' => 
         array (
             0 => __DIR__ . '/..' . '/zendframework/zend-stdlib/src',
@@ -227,9 +239,21 @@ class ComposerStaticInit2f77adf49a2df08b2cd6d1bd2b067b58
         array (
             0 => __DIR__ . '/..' . '/symfony/polyfill-ctype',
         ),
+        'Symfony\\Contracts\\Service\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/symfony/service-contracts',
+        ),
+        'Symfony\\Contracts\\Cache\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/symfony/cache-contracts',
+        ),
         'Symfony\\Component\\Yaml\\' => 
         array (
             0 => __DIR__ . '/..' . '/symfony/yaml',
+        ),
+        'Symfony\\Component\\VarExporter\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/symfony/var-exporter',
         ),
         'Symfony\\Component\\VarDumper\\' => 
         array (
@@ -291,6 +315,10 @@ class ComposerStaticInit2f77adf49a2df08b2cd6d1bd2b067b58
         array (
             0 => __DIR__ . '/..' . '/symfony/class-loader',
         ),
+        'Symfony\\Component\\Cache\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/symfony/cache',
+        ),
         'Symfony\\Cmf\\Component\\Routing\\' => 
         array (
             0 => __DIR__ . '/..' . '/symfony-cmf/routing',
@@ -298,6 +326,10 @@ class ComposerStaticInit2f77adf49a2df08b2cd6d1bd2b067b58
         'Symfony\\Bridge\\PsrHttpMessage\\' => 
         array (
             0 => __DIR__ . '/..' . '/symfony/psr-http-message-bridge',
+        ),
+        'Solarium\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/solarium/solarium/src',
         ),
         'SelfUpdate\\' => 
         array (
@@ -323,9 +355,17 @@ class ComposerStaticInit2f77adf49a2df08b2cd6d1bd2b067b58
         array (
             0 => __DIR__ . '/..' . '/psr/container/src',
         ),
+        'Psr\\Cache\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/psr/cache/src',
+        ),
         'PhpParser\\' => 
         array (
             0 => __DIR__ . '/..' . '/nikic/php-parser/lib/PhpParser',
+        ),
+        'MyCLabs\\Enum\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/myclabs/php-enum/src',
         ),
         'Masterminds\\' => 
         array (
