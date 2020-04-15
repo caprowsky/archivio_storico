9e3442f0016b6cafc89cfb9088136eb5ce04462d
sergej <sergio.durzu@gmail.com>
2020-04-15 11:51:25 +0200

    Removed migrate plus from config

--- migrate-disable.sh
+++ migrate-disable.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 drush pm-uninstall migrando -y; drush pm-uninstall migrate_tools -y; drush pm-uninstall migrate_plus -y; drush pm-uninstall migrate_drupal -y; drush pm-uninstall migrate -y;
-drush pm-uninstall migrate_drupal_ui -y;
+drush pm-uninstall migrate_drupal_ui -y;  drush pm-uninstall migrate_plus -y;
 
