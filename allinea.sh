75eb46d4e87878249bd636b005b0cc797ae47371
caprowsky <alessandrocapra@gmail.com>
2020-04-27 17:59:05 +0200

    Lievi modifiche alle viste di gestione contenuti e al tema grafico

--- allinea.sh
+++ allinea.sh
@@ -6,10 +6,11 @@ docker-compose up -d;
 
 sleep 20
 docker exec -ti studenti_solr make create core=default -f /usr/local/bin/actions.mk
+
+sleep 20
 drush updb -y;
 drush cim -y;
 
-sleep 30
 drush cr;
 drush search-api-clear;
 drush search-api-index;
