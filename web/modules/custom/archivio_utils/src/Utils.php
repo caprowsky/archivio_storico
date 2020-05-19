57d5adb1ae2ccf15be76cf1eea68f3d411b2d67b
sergej <sergio.durzu@gmail.com>
2020-05-19 10:22:17 +0200

    Added field autore and revisore to carriera template

--- web/modules/custom/archivio_utils/src/Utils.php
+++ web/modules/custom/archivio_utils/src/Utils.php
@@ -5,4 +5,16 @@ namespace Drupal\archivio_utils;
 
 class Utils {
 
+  /**
+   * @param  \Drupal\node\Entity\Node $entity
+   * @param $field_name string
+   */
+  public function implodeValue($entity, $field_name) {
+    $values = $entity->get($field_name)->getValue();
+    foreach ($values as $value) {
+      $valori[] = $value['value'];
+    }
+    return implode(", ", $valori);
+  }
+
 }
