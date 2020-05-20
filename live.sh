461f75ef44a9c560520e4c1ff98d338680283020
caprowsky <alessandrocapra@gmail.com>
2020-05-20 07:33:04 +0200

    commento il change directory

--- live.sh
+++ live.sh
@@ -6,38 +6,38 @@ project=live
 
 
 if [ $comando = "ps" ]; then
-    cd $directory
+#    cd $directory
     docker-compose ps
 fi
 
 if [ $comando = "up" ]; then
-    cd $directory
+#    cd $directory
     docker-compose -f docker-compose.yml -f docker-compose-${project}.yml up -d
 fi
 
 if [ $comando = "stop" ]; then
-    cd $directory
+#    cd $directory
     docker-compose stop
 fi
 
 if [ $comando = "restart" ]; then
-    cd $directory
+#    cd $directory
     docker-compose stop
     docker-compose -f docker-compose.yml -f docker-compose-${project}.yml up -d
 fi
 
 if [ $comando = "down" ]; then
-    cd $directory
+#    cd $directory
     docker-compose down
 fi
 
 if [ $comando = "pull" ]; then
-    cd $directory
+#    cd $directory
     docker-compose pull
 fi
 
 
 if [ $comando = "exec" ]; then
-    cd $directory
+#    cd $directory
     docker-compose exec php /bin/bash
 fi
