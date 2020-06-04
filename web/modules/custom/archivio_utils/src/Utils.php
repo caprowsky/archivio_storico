23b37a62d8d9114e5542a7c77dcc277575e676e6
sergej <sergio.durzu@gmail.com>
2020-06-04 11:08:46 +0200

    Added mappa per facoltà

--- web/modules/custom/archivio_utils/src/Utils.php
+++ web/modules/custom/archivio_utils/src/Utils.php
@@ -17,4 +17,14 @@ class Utils {
     return implode(", ", $valori);
   }
 
+  public function checkFigliDalPadre($term_id) {
+    $query = \Drupal::entityQuery('taxonomy_term')->condition('parent', $term_id);
+    return $query->execute();
+  }
+
+  public function checkPadreDalFiglio($term) {
+    $padre = $term->get('parent')->getValue();
+    return $padre[0]['target_id'];
+  }
 }
+
